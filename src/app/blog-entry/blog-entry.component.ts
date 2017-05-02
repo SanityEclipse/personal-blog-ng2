bb6ec3a3c37dc19d7e29e1e053627291770ed73b
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2017-05-02 11:23:30 -0600

    basic interpolation for blog entries implemented.

--- src/app/blog-entry/blog-entry.component.ts
+++ src/app/blog-entry/blog-entry.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Entry } from '../entries';
+
 @Component({
   selector: 'app-blog-entry',
   templateUrl: './blog-entry.component.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogEntryComponent implements OnInit {
 
+  entries = [
+    new Entry('blog 1', '11/21/2012', 'blog content goes here'),
+    new Entry('blog 2', '01/01/2014', 'blog content goes here'),
+    new Entry('blog 3', '05/7/2017', 'blog content goes here')
+
+  ];
+
+
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
---
+++ src/app/entries.ts
@@ -0,0 +1,8 @@
+export class Entry {
+
+  constructor(
+    public title: string,
+    public date: string,
+    public content: string){}
+
+}
